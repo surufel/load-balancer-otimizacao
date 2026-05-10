2cadd47861cdb9f41ce14704ebd684a132b1c273
surufel <henriquefrancasmm@gmail.com>
2026-05-10 00:36:56 -0300

    feat: PacketRule class & construtor

---
+++ src/PacketRule.java
@@ -0,0 +1,13 @@
+public class PacketRule{
+    int id;
+    String IP_source;
+    String IP_dest;
+    int priority;
+
+    public PacketRule(int id, String IP_source, String IP_dest, int priority){
+            this.id = id;
+            this.IP_source = IP_source;
+            this.IP_dest = IP_dest;
+            this.priority = priority;
+        }
+    }
