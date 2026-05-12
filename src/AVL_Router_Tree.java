c767daed62fd066832e2de8a6f13c7c0753df388
surufel <henriquefrancasmm@gmail.com>
2026-05-12 01:24:46 -0300

    add: func obter_altura da AVL + pseudocode

--- src/AVL_Router_Tree.java
+++ src/AVL_Router_Tree.java
@@ -1,3 +1,23 @@
 public class AVL_Router_Tree {
 
+    private Node root;
+
+    /*
+    Para efeito de entendimento e acesso rápido a informação,
+     vou deixar o pseudocódigo aprendido em aula.
+     */
+
+    /*
+    Função obter_altura(no):
+        se no == nulo: retorne -1
+        retorne no.altura
+    */
+
+    private int obter_altura(Node no){
+        if(no == null){
+            return -1;
+        }
+        return no.height;
+    }
+
 }
