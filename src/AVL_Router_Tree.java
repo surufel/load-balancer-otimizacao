349707789149dad846dfc67d8de0499a41cbff3e
surufel <henriquefrancasmm@gmail.com>
2026-05-12 01:26:05 -0300

    add: func calcular FB(Fator de Balanceamento)

--- src/AVL_Router_Tree.java
+++ src/AVL_Router_Tree.java
@@ -20,4 +20,16 @@ public class AVL_Router_Tree {
         return no.height;
     }
 
+    /*
+    Função calcular_FB(no):
+        se no == nulo: retorne 0
+        retorne obter_altura(no.esquerda) - obter_altura(no.direita)
+    */
+
+    private int calcular_FB(Node no){ // Fator de Balanceamento (FB = h(esq) - h(dir))
+        if(no == null){
+            return 0;
+        }
+        return obter_altura(no.left) - obter_altura(no.right);
+    }
 }
