e6b7036cd43ffb7ca226809c154d8669ac44efcb
surufel <henriquefrancasmm@gmail.com>
2026-05-23 20:59:28 -0300

    feat: delete RBT method (transplantar feito, deleteFixup a fazer)

--- src/RedBlack_Router_Tree.java
+++ src/RedBlack_Router_Tree.java
@@ -160,7 +160,55 @@ public class RedBlack_Router_Tree{
         }
     }
 
-    private void delete(){
+    private void transplantar(NodeRBT u, NodeRBT v){ // substitui uma subárvore por outra, é necessário pro delete
+        if(u.parent == nil){
+            root = v;
+        } else if(u == u.parent.left){
+            u.parent.left = v;
+        } else{
+            u.parent.right = v;
+            v.parent = u.parent;
+        }
+    }
 
+    public void delete(int id){
+        NodeRBT z = search(root, id);
+        if(z == nil){
+            return;
+        }
+        NodeRBT y = z;
+        NodeRBT x;
+        boolean originalColor = y.color;
+
+        // zero ou um filho
+        if(z.left == nil){
+            x = z.right;
+            transplantar(z, z.right);
+        } else if(z.right == nil){
+            x = z.left;
+            transplantar(z, z.left);
+        } else{
+            // dois filhos, encontra sucessor
+            y = menorNo(z.right);
+            originalColor = y.color;
+            x = y.right;
+
+            if(y.parent == z){
+                x.parent = y;
+            } else{
+                transplantar(y, y.right);
+                y.right = z.right;
+                y.right.parent = y;
+            }
+
+            transplantar(z, y);
+            y.left = z.left;
+            y.left.parent = y;
+            y.color = z.color;
+        }
+
+        // Se o nó removido era PRETO, é necessário corrigir
+        if (originalColor == black)
+            deleteFixup(x);
     }
 }
