96f3f26e588d8fec4789cfd37b23365f9582e3f8
surufel <henriquefrancasmm@gmail.com>
2026-05-23 19:53:38 -0300

    feat.: cores RBT (red,black); add: construtor inicializa pai vazio (preto)

--- src/RedBlack_Router_Tree.java
+++ src/RedBlack_Router_Tree.java
@@ -1,7 +1,12 @@
 public class RedBlack_Router_Tree{
+    private final boolean red = true;
+    private final boolean black = false;
+    private NodeRBT nil;
+    private NodeRBT root;
 
-    public RedBlack_Router_Tree(){
-
+    public RedBlack_Router_Tree(NodeRBT nil, NodeRBT root){
+        nil = new NodeRBT(null, black);
+        root = nil;
     }
 
     private void insert(){
