dd14689b01a5890db45bd579c5590df10cbe0be2
zaast <bielrlmenezes@gmail.com>
2026-05-26 08:42:51 -0300

    criando a estrutura dos testes de stress da árvore RBT

---
+++ test/RedBlack_Router_TreeTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+import java.util.Locale;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RedBlack_Router_TreeTest {
+
+    @Test
+    void realizarStressTest() {
+
+    }
+
+    @Test
+    void realizarBenchmarkTest() {
+
+    }
+}
