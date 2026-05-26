3a4f542a1d75db82a49b2a284a97af32efa135d4
zaast <bielrlmenezes@gmail.com>
2026-05-26 10:27:49 -0300

    Instanciando a árvore AVL e criando a inserção nos testes

--- test/AVL_Router_TreeTest.java
+++ test/AVL_Router_TreeTest.java
@@ -7,7 +7,17 @@ class AVL_Router_TreeTest {
 
     @Test
     void realizarStressTest() {
+        int[] volumes = {10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000};
 
+        for (int N : volumes) {
+            // Instanciando a árvore AVL
+            AVL_Router_Tree arvore = new AVL_Router_Tree();
+            // For usado para criar a inserção na árvore
+            for (int i = 1; i <= N; i++) {
+                arvore.insert(new PacketRule(i, "Rule " + i,"destino " + i ,1));
+
+            }
+        }
     }
 
     @Test
