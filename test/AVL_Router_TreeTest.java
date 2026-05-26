6e029787e81954d7336c487b00a50f13549e85c0
zaast <bielrlmenezes@gmail.com>
2026-05-26 08:15:14 -0300

    criando a estrutura dos testes de stress

---
+++ test/AVL_Router_TreeTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class AVL_Router_TreeTest {
+
+    @org.junit.jupiter.api.Test
+    void insert() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void search() {
+
+    }
+}
